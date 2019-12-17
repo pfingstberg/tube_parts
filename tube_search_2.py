a41d249a65f0e767ce113a5c4b9fe134816327ae
Pfingstberg <pfingstberg@gmx.net>
2019-12-17 11:31:34 +0100

    dodanie kanału

--- tube_search_2.py
+++ tube_search_2.py
@@ -14,21 +14,24 @@ video_list   = list()
 url_base = 'https://www.googleapis.com/youtube/v3/search?'
 
 #%%
-part       = '&part='+'snippet'
-channelId  = '&channelID='+'UCQpBmjL9kJ768fty4tbqa6Q'
+part       = '&part='+'snippet'     # 'id,snippet' daje to samo co 'snippet'
+#channelId  = '&channelId='+'UCQpBmjL9kJ768fty4tbqa6Q'   # Phil Konieczny
+channelId  = '&channelId=' +'UCUj7z3B90RqN5g8ctpLeDkw'  # Eliza Michalik
 maxResults = '&maxResults='+'8'
-order      = '&order='+'date'
+order      = '&order='     +'date'
 
 url1 = url_base+part+channelId+maxResults+order+api_key
 url2 = url_base+api_key+channelId+part+order+maxResults
 
-url3 = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyAfJGu_O13VGxcKjd90eL0A0ddAM72QEVc&channelId=UCQpBmjL9kJ768fty4tbqa6Q&part=snippet,id&order=date&maxResults=5'
+
+#url4 = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyAfJGu_O13VGxcKjd90eL0A0ddAM72QEVc&channelId=UCQpBmjL9kJ768fty4tbqa6Q&part=snippet,id&order=date&maxResults=5'
+url4 = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyAfJGu_O13VGxcKjd90eL0A0ddAM72QEVc&channelId=UCUj7z3B90RqN5g8ctpLeDkw&part=snippet,id&order=date&maxResults=5'
 
 #%%
 import certifi
 import urllib3
 http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-r = http.request('GET', url3)
+r = http.request('GET', url2)
 print(r.status,"\n"*5)
 #%%
 print(r.data)   
